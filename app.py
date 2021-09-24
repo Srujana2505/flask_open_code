b43a21525f9bdd619b83eb0b609beb61d6cb888e
Mitul16 <Mitul16@users.noreply.github.com>
2021-09-24 21:50:55 +0530

    Added POST method and input box
    I know your SECRET now

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
 from flask import Flask, request, render_template, render_template_string
+import textwrap
 
 app = Flask(__name__)
+
+# not a SECRET anymore, haha
 app.secret_key = 'shh! this is a secret'
 
-@app.route('/')
-def hello_world():
+
+def dedent(inner):
+    def wrapper():
+        return textwrap.dedent(inner()).strip()
+
+    return wrapper
+
+
+@app.route('/', endpoint='index')
+def index():
     return render_template('home.html')
-@app.route('/chal_one/')
+
+
+@app.route('/chal_one/', endpoint='hello_name', methods=['GET', 'POST'])
+@dedent
 def hello_name():
-    name = request.args.get('name')
+    if request.method == 'POST':
+        name = request.form.get('name', None)
 
-    template = '''
-        <h1>Hello {}</h1>
-    '''.format(name)
+        if name:
+            template = f'''
+                <h1>Hello {name}</h1>
+            '''
 
-    return render_template_string(template)
+            return render_template_string(template)
+
+    return '''
+        <form action="" method="POST">
+            <h3 style="margin: 0px;">Exploit me</h3>
+            <input type="text" name="name" value="" placeholder"enter your name"/>
+            <input type="submit" name="submit" value="submit"/>
+        </form>
+    '''
 
 
-        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="1337")
